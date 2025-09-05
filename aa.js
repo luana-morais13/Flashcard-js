6bb813418dba43162cd1a71525d589efad795f7e
Luana <luana.morais1305@gmail.com>
2025-09-05 10:40:16 -0300

    Add files via upload
    Primeiro commit

---
+++ aa.js
@@ -0,0 +1,7 @@
+function showCard() {
+    document.getElementById('cartao').style.display = 'flex'; // Exibe e centraliza o quadro
+}
+
+function hideCard() {
+    document.getElementById('cartao').style.display = 'none'; // Esconde o quadro de criação
+}
